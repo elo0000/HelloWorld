4e8e9684344fc34416cc771313e4794ef072f248
Edmund Lo <edmund.lo@safeway.com>
2016-11-22 10:28:02 -0800

    test

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ Public class HelloWorld
   Public static void main (String [] args)
   {
     System.out.println("Java Hello World");
-    System.out.println("Java Rock !!!");
+    System.out.println("Java Rocks !!!");
   }
 }
     
