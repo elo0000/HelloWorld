f579081cfa9ab8360ec48816195423db384edda3
Edmund Lo <edmund.lo@safeway.com>
2016-11-22 13:02:27 -0800

    test tag

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld
     System.out.println("Java Hello World");
     System.out.println("Java Rocks !!!");
     System.out.println("Java Superhero !!!");
+    System.out.println("Tag Tag Tag !!!");
   }
 }
     
