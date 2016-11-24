d4289bc52ea1f8ff15749494533fc34c72d96a5b
Edmund Lo <edmund.lo@safeway.com>
2016-11-24 10:32:40 -0800

    local build

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,9 +4,7 @@ public class HelloWorld
   {
     System.out.println("Java Hello World");
     System.out.println("Java Rocks !!!");
-    System.out.println("Java Superhero !!!");
-    System.out.println("Tag Tag Tag !!!");
-    System.out.println("Fail Tag !!!");
+    System.out.println("Local Build !!!");
   }
 }
     
